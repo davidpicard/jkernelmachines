41da03f9f76c7dbbcbef002812f67fb9e1699eee
David Picard <picard@ensea.fr>
2012-05-31 14:08:43 +0200

    adding a test for leave one out cv, plus some cleaning

--- src/fr/lip6/test/evaluation/TestCrossValidation.java
+++ src/fr/lip6/test/evaluation/TestCrossValidation.java
@@ -2,11 +2,14 @@ package fr.lip6.test.evaluation;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
+import fr.lip6.classifier.Classifier;
 import fr.lip6.classifier.LaSVM;
 import fr.lip6.evaluation.AccuracyEvaluator;
 import fr.lip6.evaluation.Evaluator;
+import fr.lip6.evaluation.LeaveOneOutCrossValidation;
 import fr.lip6.evaluation.RandomSplitCrossValidation;
 import fr.lip6.kernel.typed.DoubleGaussL2;
 import fr.lip6.type.TrainingSample;
@@ -17,57 +20,90 @@ public class TestCrossValidation {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		int dimension = 3;
-		int nbPosTrain = 500;
-		int nbNegTrain = 500;
-		double p = 2.0;
-		
+		int nbPosTrain = 50;
+		int nbNegTrain = 50;
+		double p = 1.5;
+
 		Random ran = new Random(System.currentTimeMillis());
-		
+
 		ArrayList<TrainingSample<double[]>> train = new ArrayList<TrainingSample<double[]>>();
-		//1. generate positive train samples
-		for(int i = 0 ; i < nbPosTrain; i++)
-		{
+		// 1. generate positive train samples
+		for (int i = 0; i < nbPosTrain; i++) {
 			double[] t = new double[dimension];
-			for(int x = 0 ; x < dimension; x++)
-			{
-				t[x] = -p + ran.nextGaussian();
+			for (int x = 0; x < dimension; x++) {
+				t[x] = -p + ran.nextDouble();
 			}
-			
+
 			train.add(new TrainingSample<double[]>(t, 1));
 		}
-		//2. generate negative train samples
-		for(int i = 0 ; i < nbNegTrain; i++)
-		{
+		// 2. generate negative train samples
+		for (int i = 0; i < nbNegTrain; i++) {
 			double[] t = new double[dimension];
-			for(int x = 0 ; x < dimension; x++)
-			{
-				t[x] = p + ran.nextGaussian();
+			for (int x = 0; x < dimension; x++) {
+				t[x] = p + ran.nextDouble();
 			}
-			
+
 			train.add(new TrainingSample<double[]>(t, -1));
 		}
-		
-		//3. instanciate classifier
+
+		// 3. instanciate classifier
 		DoubleGaussL2 k = new DoubleGaussL2();
 		k.setGamma(1.0);
 		LaSVM<double[]> svm = new LaSVM<double[]>(k);
 		svm.setC(100);
 		
-		//4. CrossValidation
-		Evaluator<double[]> eval = new AccuracyEvaluator<double[]>();
-		RandomSplitCrossValidation<double[]> cv = new RandomSplitCrossValidation<double[]>(svm, train, eval);
-		cv.setTrainPercent(0.75);
-		cv.setNbTest(25);
-		
-		//5. perfom tests
-		cv.run();
-		
-		//6. get results
-		System.out.println("Accuracy: "+cv.getAverageScore()+" +/- "+cv.getStdDevScore());
-		System.out.println("(scores: "+Arrays.toString(cv.getScores())+")");
+		if(!testRandomSplitCrossValidation(svm, train))
+			System.err.println("WARNING: RandomSplitCrossValidation failed!");
+		if(!testLeaveOneOutCrossValidation(svm, train))
+			System.err.println("WARNING: LeaveOneOutCrossValidation failed!");
 
 	}
 
+	/*
+	 * test RandomSplitCrossValidation class
+	 */
+	private static boolean testRandomSplitCrossValidation(
+			Classifier<double[]> c, List<TrainingSample<double[]>> l) {
+		
+		// 4. CrossValidation
+		Evaluator<double[]> eval = new AccuracyEvaluator<double[]>();
+		RandomSplitCrossValidation<double[]> cv = new RandomSplitCrossValidation<double[]>(
+				c, l, eval);
+		cv.setTrainPercent(0.80);
+		cv.setNbTest(10);
+
+		// 5. perfom tests
+		cv.run();
+
+		// 6. get results
+		System.out.println("Accuracy: " + cv.getAverageScore() + " +/- "
+				+ cv.getStdDevScore());
+		System.out.println("(scores: " + Arrays.toString(cv.getScores()) + ")");
+		
+		return (cv.getAverageScore()==1.0);
+	}
+
+	/*
+	 * test LeaveOneOutCrossValidation class
+	 */
+	private static boolean testLeaveOneOutCrossValidation(
+			Classifier<double[]> c, List<TrainingSample<double[]>> l) {
+		
+		// 4. CrossValidation
+		Evaluator<double[]> eval = new AccuracyEvaluator<double[]>();
+		LeaveOneOutCrossValidation<double[]> cv = new LeaveOneOutCrossValidation<double[]>(
+				c, l, eval);
+
+		// 5. perfom tests
+		cv.run();
+
+		// 6. get results
+		System.out.println("Accuracy: " + cv.getAverageScore() + " +/- "
+				+ cv.getStdDevScore());
+		System.out.println("(scores: " + Arrays.toString(cv.getScores()) + ")");
+		
+		return (cv.getAverageScore()==1.0);
+	}
 }
