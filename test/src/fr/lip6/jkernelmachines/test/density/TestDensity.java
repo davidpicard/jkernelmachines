2c87053367613de15ced58606d889febc12fdcc6
David Picard <picard@ensea.fr>
2014-04-17 18:33:47 +0200

    various test on density, but deprecated. will be removed shortly

--- test/src/fr/lip6/jkernelmachines/test/density/TestDensity.java
+++ test/src/fr/lip6/jkernelmachines/test/density/TestDensity.java
@@ -16,20 +16,26 @@
 
     Copyright David Picard - 2010
 
-*/
+ */
 package fr.lip6.jkernelmachines.test.density;
 
 import java.util.ArrayList;
 import java.util.Random;
 
+import fr.lip6.jkernelmachines.density.DoubleGaussianMixtureModel;
 import fr.lip6.jkernelmachines.density.ParzenDensity;
+import fr.lip6.jkernelmachines.density.SDCADensity;
 import fr.lip6.jkernelmachines.density.SMODensity;
+import fr.lip6.jkernelmachines.density.SimpleMKLDensity;
 import fr.lip6.jkernelmachines.kernel.typed.DoubleGaussL2;
+import fr.lip6.jkernelmachines.kernel.typed.index.IndexDoubleGaussL2;
+import fr.lip6.jkernelmachines.util.DebugPrinter;
 
 /**
  * Test cases of density estimators using generated data.
+ * 
  * @author picard
- *
+ * 
  */
 @Deprecated
 public class TestDensity {
@@ -41,72 +47,83 @@ public class TestDensity {
 		int dimension = 3;
 		int nbPosTrain = 250;
 		int nbPosTest = 25;
-		
+
 		Random ran = new Random(System.currentTimeMillis());
-		
+
 		ArrayList<double[]> train = new ArrayList<double[]>();
-		//1. generate positive train samples
-		for(int i = 0 ; i < nbPosTrain; i++)
-		{
+		// 1. generate positive train samples
+		for (int i = 0; i < nbPosTrain; i++) {
 			double[] t = new double[dimension];
-			for(int x = 0 ; x < dimension; x++)
-			{
+			for (int x = 0; x < dimension; x++) {
 				t[x] = ran.nextGaussian();
 			}
-			
+
 			train.add(t);
 		}
 		System.out.println("Samples generated");
-		
-		
-		//3. train svm
+
+		// 3. train svm
 		long time = System.currentTimeMillis();
-//		Kernel<double[]> k = new DoubleLinear();
+		// Kernel<double[]> k = new DoubleLinear();
 		DoubleGaussL2 k = new DoubleGaussL2();
 		k.setGamma(2);
 		SMODensity<double[]> svm = new SMODensity<double[]>(k);
 		svm.setC(100);
 		svm.train(train);
 		long smotime = System.currentTimeMillis() - time;
-		System.out.println("SMO done in "+smotime);
-		
+		System.out.println("SMO done in " + smotime);
+
 		ParzenDensity<double[]> parzen = new ParzenDensity<double[]>(k);
 		parzen.train(train);
-		
-		
-		
+
+		SDCADensity<double[]> sdca = new SDCADensity<double[]>(k);
+		sdca.setC(100);
+		sdca.train(train);
+
+		DoubleGaussianMixtureModel gmm = new DoubleGaussianMixtureModel(1);
+		gmm.train(train);
+
+		SimpleMKLDensity<double[]> mkl = new SimpleMKLDensity<double[]>();
+		for (int x = 0; x < dimension; x++) {
+			mkl.addKernel(new IndexDoubleGaussL2(x));
+		}
+		mkl.setC(100);
+		mkl.train(train);
+
 		ArrayList<double[]> test = new ArrayList<double[]>();
-		//4. generate positive test samples
-		for(int i = 0 ; i < nbPosTest; i++)
-		{
+		// 4. generate positive test samples
+		for (int i = 0; i < nbPosTest; i++) {
 			double[] t = new double[dimension];
-			for(int x = 0 ; x < dimension; x++)
-			{
+			for (int x = 0; x < dimension; x++) {
 				t[x] = ran.nextGaussian();
 			}
-			
+
 			test.add(t);
 		}
-		
-		//6. test svm
-		for(double[] t : test)
-		{
+
+		// 6. test svm
+		for (double[] t : test) {
 			double value = svm.valueOf(t);
 			double pvalue = parzen.valueOf(t);
-			
-			System.out.println("smo value : "+value+", parzen value : "+pvalue);
-			
-			
+			double dvalue = sdca.valueOf(t);
+			double gvalue = gmm.valueOf(t);
+			double mvalue = mkl.valueOf(t);
+
+			System.out.println("smo: " + value + ", parzen: "
+					+ pvalue + ", sdca: " + dvalue + " , gmm: "
+					+ gvalue + " , mkl: " + mvalue);
+
 		}
-		
+
 		double[] alphas = svm.getAlphas();
 		int nnz = 0;
-		for(double d : alphas)
-			if(d > 0)
+		for (double d : alphas)
+			if (d > 0)
 				nnz++;
-				
-		System.out.println("Non zeros coefficients : "+nnz+" ("+(nnz/(double)alphas.length)+")");
-		
+
+		System.out.println("Non zeros coefficients : " + nnz + " ("
+				+ (nnz / (double) alphas.length) + ")");
+
 	}
 
 }
