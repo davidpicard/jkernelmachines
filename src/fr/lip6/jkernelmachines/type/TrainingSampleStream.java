2139b55ed3795cbe4a0b675142f46551448e2a44
David Picard <picard@ensea.fr>
2016-02-16 16:46:19 +0100

    add framework for true online learning

---
+++ src/fr/lip6/jkernelmachines/type/TrainingSampleStream.java
@@ -0,0 +1,36 @@
+/**
+    This file is part of JkernelMachines.
+
+    JkernelMachines is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JkernelMachines is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JkernelMachines.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright David Picard - 2014
+
+*/
+package fr.lip6.jkernelmachines.type;
+
+/**
+ * Interface to streams of training samples, useful for online training.
+ * 
+ * @author picard
+ *
+ */
+public interface TrainingSampleStream<T> {
+	
+	/**
+	 * Return the next training sample from this stream
+	 * @return null if the stream has no more elements
+	 */
+	public TrainingSample<T> nextSample();
+
+}
