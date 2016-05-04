aee0818692cd445bbfa6997ae2d29430a357511f
David Picard <picard@ensea.fr>
2016-05-04 16:34:21 +0200

    avoid nan in normalization

--- src/main/java/net/jkernelmachines/util/DataPreProcessing.java
+++ src/main/java/net/jkernelmachines/util/DataPreProcessing.java
@@ -36,109 +36,118 @@ import net.jkernelmachines.type.TrainingSample;
 
 /**
  * Util class for doing some preprocessing on data
+ * 
  * @author picard
  *
  */
 public class DataPreProcessing {
-	
+
 	/**
-	 * Normalize a list of training samples of double[] to have l2-norm equal to 1
-	 * @param list list
+	 * Normalize a list of training samples of double[] to have l2-norm equal to
+	 * 1
+	 * 
+	 * @param list
+	 *            list
 	 */
 	public static void normalizeList(List<TrainingSample<double[]>> list) {
-		if(list.isEmpty())
+		if (list.isEmpty())
 			return;
 		DoubleLinear linear = new DoubleLinear();
-		
-		for(TrainingSample<double[]> t : list) {
+
+		for (TrainingSample<double[]> t : list) {
 			double[] desc = t.sample;
 			double norm = Math.sqrt(linear.valueOf(desc, desc));
-			for(int x = 0 ; x < desc.length ; x++)
-				desc[x] /= norm;
+			if (norm > 0) {
+				for (int x = 0; x < desc.length; x++)
+					desc[x] /= norm;
+			}
 		}
-		
+
 	}
-	
 
 	/**
 	 * Normalize a list of double[] to have l2-norm equal to 1
-	 * @param list list
+	 * 
+	 * @param list
+	 *            list
 	 */
 	public static void normalizeDoubleList(List<double[]> list) {
-		if(list.isEmpty())
+		if (list.isEmpty())
 			return;
 		DoubleLinear linear = new DoubleLinear();
-		
-		for(double[] desc : list) {
+
+		for (double[] desc : list) {
 			double norm = Math.sqrt(linear.valueOf(desc, desc));
-			for(int x = 0 ; x < desc.length ; x++)
+			for (int x = 0; x < desc.length; x++)
 				desc[x] /= norm;
 		}
-		
+
 	}
 
 	/**
 	 * Process a list of training samples of double[] to have 0 mean
-	 * @param list list
+	 * 
+	 * @param list
+	 *            list
 	 */
 	public static void centerList(List<TrainingSample<double[]>> list) {
-		if(list.isEmpty())
+		if (list.isEmpty())
 			return;
-		
+
 		double[] mean = new double[list.get(0).sample.length];
-		for(TrainingSample<double[]> t : list) {
+		for (TrainingSample<double[]> t : list) {
 			double[] d = t.sample;
-			for(int x = 0 ; x < d.length ; x++) {
+			for (int x = 0; x < d.length; x++) {
 				mean[x] += d[x];
 			}
 		}
 
-		for(int x = 0 ; x < mean.length ; x++)
+		for (int x = 0; x < mean.length; x++)
 			mean[x] /= list.size();
-		
-		for(TrainingSample<double[]> t : list) {
+
+		for (TrainingSample<double[]> t : list) {
 			double[] d = t.sample;
-			for(int x = 0 ; x < d.length ; x++) {
+			for (int x = 0; x < d.length; x++) {
 				d[x] -= mean[x];
 			}
 		}
 	}
-	
+
 	/**
 	 * Process a list of samples of double[] to have unit variance
-	 * @param list list
+	 * 
+	 * @param list
+	 *            list
 	 */
 	public static void reduceList(List<TrainingSample<double[]>> list) {
-		if(list.isEmpty())
+		if (list.isEmpty())
 			return;
-		
+
 		double[] mean = new double[list.get(0).sample.length];
 		double[] square = new double[mean.length];
 		double[] factor = new double[square.length];
-		for(TrainingSample<double[]> t : list) {
+		for (TrainingSample<double[]> t : list) {
 			double[] d = t.sample;
-			for(int x = 0 ; x < d.length ; x++) {
+			for (int x = 0; x < d.length; x++) {
 				mean[x] += d[x];
-				square[x] += d[x]*d[x];
+				square[x] += d[x] * d[x];
 			}
 		}
 
-		for(int x = 0 ; x < mean.length ; x++) {
+		for (int x = 0; x < mean.length; x++) {
 			mean[x] /= list.size();
 			square[x] /= list.size();
-			factor[x] = Math.sqrt((square[x] - mean[x]*mean[x]));
-			if(factor[x] == 0)
+			factor[x] = Math.sqrt((square[x] - mean[x] * mean[x]));
+			if (factor[x] == 0)
 				factor[x] = 1.0;
 		}
-		
-		for(TrainingSample<double[]> t : list) {
+
+		for (TrainingSample<double[]> t : list) {
 			double[] d = t.sample;
-			for(int x = 0 ; x < d.length ; x++) {
+			for (int x = 0; x < d.length; x++) {
 				d[x] /= factor[x];
 			}
 		}
-				
-		
-		
+
 	}
 }
